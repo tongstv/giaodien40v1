0f1ee5960edf9fc900cfc8cdfa832e0ce47d46d2
Tongstv <tongstv@gmail.com>
2020-02-26 22:06:35 +0700

    dung

---
+++ bangso/simnamsinh.blade.php
@@ -0,0 +1,185 @@
+<!-- search sim nam sinh -->
+<div class="panel row no-gutters">
+    <div class="panel-content">
+        <div class="box-search-simns">
+            <h3 class="title text-center">TÌM SIM THEO NGÀY SINH</h3>
+            <form id="search_sim_ns" action="" method="post">
+                <label>Ngày / Tháng / Năm</label>
+                <div class="input-group row">
+                    <div class="col-sm-4">
+                        <select name="ngay">
+                            <option value="1">01</option>
+                            <option value="2">01</option>
+                            <option value="3">01</option>
+                            <option value="4">01</option>
+                            <option value="5">01</option>
+                            <option value="6">01</option>
+                            <option value="7">01</option>
+                            <option value="8">01</option>
+                            <option value="9">01</option>
+                            <option value="10">01</option>
+                            <option value="11">01</option>
+                            <option value="12">01</option>
+                            <option value="13">01</option>
+                            <option value="14">01</option>
+                            <option value="15">01</option>
+                            <option value="16">01</option>
+                            <option value="17">01</option>
+                            <option value="18">01</option>
+                            <option value="19">01</option>
+                            <option value="20">01</option>
+                            <option value="21">01</option>
+                            <option value="22">01</option>
+                            <option value="23">01</option>
+                            <option value="24">01</option>
+                            <option value="25">01</option>
+                            <option value="26">01</option>
+                            <option value="27">01</option>
+                            <option value="28">01</option>
+                            <option value="29">01</option>
+                            <option value="30">01</option>
+                            <option value="31">01</option>
+                        </select>
+                    </div>
+                    <div class="col-sm-4">
+                        <select name="thang">
+                            <option value="1">01</option>
+                            <option value="2">02</option>
+                            <option value="3">03</option>
+                            <option value="4">04</option>
+                            <option value="5">05</option>
+                            <option value="6">06</option>
+                            <option value="7">07</option>
+                            <option value="8">08</option>
+                            <option value="9">09</option>
+                            <option value="10">10</option>
+                            <option value="11">11</option>
+                            <option value="12">12</option>
+                        </select>
+                    </div>
+                    <div class="col-sm-4">
+                        <select name="nam">
+                            <option value="1">1972</option>
+                            <option value="2">1973</option>
+                            <option value="3">1974</option>
+                            <option value="4">1975</option>
+                            <option value="5">1976</option>
+                            <option value="6">1977</option>
+                            <option value="7">1978</option>
+                            <option value="8">1979</option>
+                            <option value="9">1980</option>
+                            <option value="10">1981</option>
+                            <option value="11">1982</option>
+                            <option value="12">1983</option>
+                            <option value="13">1984</option>
+                            <option value="14">1985</option>
+                            <option value="15">1986</option>
+                            <option value="16">1987</option>
+                            <option value="17">1988</option>
+                            <option value="18">1989</option>
+                            <option value="19">1990</option>
+                            <option value="20">1991</option>
+                            <option value="21">1992</option>
+                            <option value="22">1993</option>
+                            <option value="23">1994</option>
+                            <option value="24">1995</option>
+                            <option value="25">1996</option>
+                            <option value="26">1997</option>
+                            <option value="27">1998</option>
+                            <option value="28">1999</option>
+                            <option value="29">2000</option>
+                            <option value="30">2001</option>
+                            <option value="31">2002</option>
+                            <option value="32">2003</option>
+                            <option value="33">2004</option>
+                            <option value="34">2005</option>
+                            <option value="35">2006</option>
+                            <option value="36">2007</option>
+                            <option value="37">2008</option>
+                            <option value="38">2009</option>
+                            <option value="39">2010</option>
+                            <option value="40">2011</option>
+                            <option value="41">2012</option>
+                            <option value="42">2013</option>
+                            <option value="43">2014</option>
+                            <option value="44">2015</option>
+                            <option value="45">2016</option>
+                            <option value="46">2017</option>
+                            <option value="47">2018</option>
+                            <option value="48">2019</option>
+                        </select>
+                    </div>
+                </div>
+            </form>
+        </div>
+
+    </div>
+</div>
+<!-- panel -->
+<div class="panel row no-gutters">
+    <div class="panel-header text-center">TÌM SIM THEO NĂM SINH</div>
+    <div class="panel-content">
+        <div class="box-sim">
+            <a href="danh-muc.html">Sim năm sinh 1971</a>
+            <a href="danh-muc.html">Sim năm sinh 1972</a>
+            <a href="danh-muc.html">Sim năm sinh 1973</a>
+            <a href="danh-muc.html">Sim năm sinh 1974</a>
+            <a href="danh-muc.html">Sim năm sinh 1975</a>
+            <a href="danh-muc.html">Sim năm sinh 1976</a>
+            <a href="danh-muc.html">Sim năm sinh 1977</a>
+            <a href="danh-muc.html">Sim năm sinh 1978</a>
+            <a href="danh-muc.html">Sim năm sinh 1979</a>
+            <a href="danh-muc.html">Sim năm sinh 1980</a>
+            <a href="danh-muc.html">Sim năm sinh 1981</a>
+            <a href="danh-muc.html">Sim năm sinh 1982</a>
+            <a href="danh-muc.html">Sim năm sinh 1983</a>
+            <a href="danh-muc.html">Sim năm sinh 1984</a>
+            <a href="danh-muc.html">Sim năm sinh 1985</a>
+            <a href="danh-muc.html">Sim năm sinh 1986</a>
+            <a href="danh-muc.html">Sim năm sinh 1987</a>
+            <a href="danh-muc.html">Sim năm sinh 1988</a>
+            <a href="danh-muc.html">Sim năm sinh 1989</a>
+            <a href="danh-muc.html">Sim năm sinh 1990</a>
+            <a href="danh-muc.html">Sim năm sinh 1991</a>
+            <a href="danh-muc.html">Sim năm sinh 1992</a>
+            <a href="danh-muc.html">Sim năm sinh 1993</a>
+            <a href="danh-muc.html">Sim năm sinh 1994</a>
+            <a href="danh-muc.html">Sim năm sinh 1995</a>
+            <a href="danh-muc.html">Sim năm sinh 1996</a>
+            <a href="danh-muc.html">Sim năm sinh 1997</a>
+            <a href="danh-muc.html">Sim năm sinh 1998</a>
+            <a href="danh-muc.html">Sim năm sinh 1999</a>
+            <a href="danh-muc.html">Sim năm sinh 2000</a>
+            <a href="danh-muc.html">Sim năm sinh 2001</a>
+            <a href="danh-muc.html">Sim năm sinh 2002</a>
+            <a href="danh-muc.html">Sim năm sinh 2003</a>
+            <a href="danh-muc.html">Sim năm sinh 2004</a>
+            <a href="danh-muc.html">Sim năm sinh 2005</a>
+            <a href="danh-muc.html">Sim năm sinh 2006</a>
+            <a href="danh-muc.html">Sim năm sinh 2007</a>
+            <a href="danh-muc.html">Sim năm sinh 2008</a>
+            <a href="danh-muc.html">Sim năm sinh 2009</a>
+            <a href="danh-muc.html">Sim năm sinh 2010</a>
+            <a href="danh-muc.html">Sim năm sinh 2011</a>
+            <a href="danh-muc.html">Sim năm sinh 2012</a>
+            <a href="danh-muc.html">Sim năm sinh 2013</a>
+            <a href="danh-muc.html">Sim năm sinh 2014</a>
+            <a href="danh-muc.html">Sim năm sinh 2015</a>
+            <a href="danh-muc.html">Sim năm sinh 2016</a>
+            <a href="danh-muc.html">Sim năm sinh 2017</a>
+            <a href="danh-muc.html">Sim năm sinh 2018</a>
+            <a href="danh-muc.html">Sim năm sinh 2019</a>
+            <a href="danh-muc.html">Sim năm sinh 2020</a>
+            <a href="danh-muc.html">Sim năm sinh 2021</a>
+            <a href="danh-muc.html">Sim năm sinh 2022</a>
+            <a href="danh-muc.html">Sim năm sinh 2023</a>
+            <a href="danh-muc.html">Sim năm sinh 2024</a>
+            <a href="danh-muc.html">Sim năm sinh 2025</a>
+            <a href="danh-muc.html">Sim năm sinh 2026</a>
+            <a href="danh-muc.html">Sim năm sinh 2027</a>
+            <a href="danh-muc.html">Sim năm sinh 2028</a>
+            <a href="danh-muc.html">Sim năm sinh 2029</a>
+            <a href="danh-muc.html">Sim năm sinh 2030</a>
+        </div>
+    </div>
+</div>
---
+++ bangso/simtragop_loc.blade.php
@@ -0,0 +1,166 @@
+<div class="text-box filter-box">
+
+    <h3 class="title">Bộ lọc sim</h3>
+
+    @if(\Illuminate\Support\Facades\Input::get('mang') == '')
+    <div class="filter-link">
+        <span class="label">Chọn đầu số:</span>
+        <!-- filter item -->
+
+
+        <a data-remove="09" @if(\Illuminate\Support\Facades\Input::get('dauso') == '09')
+
+        class="active"
+        @endif
+
+        href="{{URL::current()."?page=".(\Illuminate\Support\Facades\Input::get('page') ?? 0)."&".http_build_query(['mang' => (\Illuminate\Support\Facades\Input::get('mang') ?? '') , 'dauso' => '09', 'gia' => (\Illuminate\Support\Facades\Input::get('gia') ?? ''), 'sapxep' => (\Illuminate\Support\Facades\Input::get('sapxep') ?? ''),'sim' => (\Illuminate\Support\Facades\Input::get('sim') ?? ''),  'loai' => (\Illuminate\Support\Facades\Input::get('loai') ?? '')])}}">09</a>
+        <a data-remove="08"
+           @if(\Illuminate\Support\Facades\Input::get('dauso') == '08')
+
+        class="active"
+        @endif
+        href="{{URL::current()."?page=".(\Illuminate\Support\Facades\Input::get('page') ? '' :  0)."&".http_build_query(['mang' => (\Illuminate\Support\Facades\Input::get('mang') ?? '') , 'dauso' => '08', 'gia' => (\Illuminate\Support\Facades\Input::get('gia') ?? ''), 'sapxep' => (\Illuminate\Support\Facades\Input::get('sapxep') ?? ''),'sim' => (\Illuminate\Support\Facades\Input::get('sim') ?? ''),  'loai' => (\Illuminate\Support\Facades\Input::get('loai') ?? '')])}}">08</a>
+        <a data-remove="07" @if(\Illuminate\Support\Facades\Input::get('dauso') == '07')
+
+        class="active"
+        @endif
+        href="{{URL::current()."?page=".(\Illuminate\Support\Facades\Input::get('page') ?? 0)."&".http_build_query(['mang' => (\Illuminate\Support\Facades\Input::get('mang') ?? '') , 'dauso' => '07', 'gia' => (\Illuminate\Support\Facades\Input::get('gia') ?? ''), 'sapxep' => (\Illuminate\Support\Facades\Input::get('sapxep') ?? ''),'sim' => (\Illuminate\Support\Facades\Input::get('sim') ?? ''),  'loai' => (\Illuminate\Support\Facades\Input::get('loai') ?? '')])}}">07</a>
+        <a data-remove="05" @if(\Illuminate\Support\Facades\Input::get('dauso') == '05')
+
+        class="active"
+        @endif
+        href="{{URL::current()."?page=".(\Illuminate\Support\Facades\Input::get('page') ?? 0)."&".http_build_query(['mang' => (\Illuminate\Support\Facades\Input::get('mang') ?? '') , 'dauso' => '05', 'gia' => (\Illuminate\Support\Facades\Input::get('gia') ?? ''), 'sapxep' => (\Illuminate\Support\Facades\Input::get('sapxep') ?? ''),'sim' => (\Illuminate\Support\Facades\Input::get('sim') ?? ''),  'loai' => (\Illuminate\Support\Facades\Input::get('loai') ?? '')])}}"
+        >05</a>
+        <a data-remove="03" @if(\Illuminate\Support\Facades\Input::get('dauso') == '03')
+
+        class="active"
+        @endif
+        href="{{URL::current()."?page=".(\Illuminate\Support\Facades\Input::get('page') ?? 0)."&".http_build_query(['mang' => (\Illuminate\Support\Facades\Input::get('mang') ?? '') , 'dauso' => '03', 'gia' => (\Illuminate\Support\Facades\Input::get('gia') ?? ''), 'sapxep' => (\Illuminate\Support\Facades\Input::get('sapxep') ?? ''),'sim' => (\Illuminate\Support\Facades\Input::get('sim') ?? ''),  'loai' => (\Illuminate\Support\Facades\Input::get('loai') ?? '')])}}">03</a>
+        <!-- #filter item -->
+    </div>
+    @endif
+    @if(\Illuminate\Support\Facades\Input::get('dauso') == '')
+
+    <div class="filter-link">
+        <span class="label">Chọn mạng:</span>
+        <!-- filter item -->
+        <a data-remove="viettel"
+
+           @if(\Illuminate\Support\Facades\Input::get('mang') == 'viettel')
+
+        class="active"
+        @endif
+        href="{{URL::current()."?page=".(\Illuminate\Support\Facades\Input::get('page') ?? 0)."&".http_build_query(['mang' => 'viettel' , 'dauso' =>  (\Illuminate\Support\Facades\Input::get('dauso') ?? ''), 'gia' => (\Illuminate\Support\Facades\Input::get('gia') ?? ''), 'sapxep' => (\Illuminate\Support\Facades\Input::get('sapxep') ?? ''),'sim' => (\Illuminate\Support\Facades\Input::get('sim') ?? ''),  'loai' => (\Illuminate\Support\Facades\Input::get('loai') ?? '')])}}"
+        >Viettel</a>
+        <a data-remove="mobifone" @if(\Illuminate\Support\Facades\Input::get('mang') == 'mobifone')
+
+        class="active"
+        @endif
+
+        href="{{URL::current()."?page=".(\Illuminate\Support\Facades\Input::get('page') ?? 0)."&".http_build_query(['mang' => 'mobifone' , 'dauso' =>  (\Illuminate\Support\Facades\Input::get('dauso') ?? ''), 'gia' => (\Illuminate\Support\Facades\Input::get('gia') ?? ''), 'sapxep' => (\Illuminate\Support\Facades\Input::get('sapxep') ?? ''),'sim' => (\Illuminate\Support\Facades\Input::get('sim') ?? ''),  'loai' => (\Illuminate\Support\Facades\Input::get('loai') ?? '')])}}"
+        >Mobifone</a>
+        <a data-remove="vinaphone" @if(\Illuminate\Support\Facades\Input::get('mang') == 'vinaphone')
+
+        class="active"
+        @endif href="{{URL::current()."?page=".(\Illuminate\Support\Facades\Input::get('page') ?? 0)."&".http_build_query(['mang' => 'vinaphone' , 'dauso' =>  (\Illuminate\Support\Facades\Input::get('dauso') ?? ''), 'gia' => (\Illuminate\Support\Facades\Input::get('gia') ?? ''), 'sapxep' => (\Illuminate\Support\Facades\Input::get('sapxep') ?? ''),'sim' => (\Illuminate\Support\Facades\Input::get('sim') ?? ''),  'loai' => (\Illuminate\Support\Facades\Input::get('loai') ?? '')])}}"
+        data-mang="mobifone">Vinaphone</a>
+        <a data-remove="vietnamobile" @if(\Illuminate\Support\Facades\Input::get('mang') == 'vietnamobile')
+
+        class="active"
+        @endif href="{{URL::current()."?page=".(\Illuminate\Support\Facades\Input::get('page') ?? 0)."&".http_build_query(['mang' => 'vietnamobile' , 'dauso' =>  (\Illuminate\Support\Facades\Input::get('dauso') ?? ''), 'gia' => (\Illuminate\Support\Facades\Input::get('gia') ?? ''), 'sapxep' => (\Illuminate\Support\Facades\Input::get('sapxep') ?? ''),'sim' => (\Illuminate\Support\Facades\Input::get('sim') ?? ''),  'loai' => (\Illuminate\Support\Facades\Input::get('loai') ?? '')])}}"
+        >Vietnamobile</a>
+        <a data-remove="gmobile"
+           @if(\Illuminate\Support\Facades\Input::get('mang') == 'gmobile')
+
+        class="active"
+        @endif href="{{URL::current()."?page=".(\Illuminate\Support\Facades\Input::get('page') ?? 0)."&".http_build_query(['mang' => 'gmobile' , 'dauso' =>  (\Illuminate\Support\Facades\Input::get('dauso') ?? ''), 'gia' => (\Illuminate\Support\Facades\Input::get('gia') ?? ''), 'sapxep' => (\Illuminate\Support\Facades\Input::get('sapxep') ?? ''),'sim' => (\Illuminate\Support\Facades\Input::get('sim') ?? ''),  'loai' => (\Illuminate\Support\Facades\Input::get('loai') ?? '')])}}"
+        >Gmobile</a>
+        <a data-remove="Itelecom" @if(\Illuminate\Support\Facades\Input::get('mang') == 'Itelecom')
+
+        class="active"
+        @endif href="{{URL::current()."?page=".(\Illuminate\Support\Facades\Input::get('page') ?? 0)."&".http_build_query(['mang' => 'Itelecom' , 'dauso' =>  (\Illuminate\Support\Facades\Input::get('dauso') ?? ''), 'gia' => (\Illuminate\Support\Facades\Input::get('gia') ?? ''), 'sapxep' => (\Illuminate\Support\Facades\Input::get('sapxep') ?? ''),'sim' => (\Illuminate\Support\Facades\Input::get('sim') ?? ''),  'loai' => (\Illuminate\Support\Facades\Input::get('loai') ?? '')])}}"
+        >Itelecom</a>
+        <!-- #filter item -->
+    </div>
+
+    @endif
+
+
+    <div class="filter-link">
+        <span class="label">Chọn giá:</span>
+
+        @php
+
+        $khoanggias = (object) [
+
+        '0-1' => '< 1tr',
+        '1-3' => '1-3tr',
+        '3-5' => '3-5tr',
+        '5-10' => '5-10tr',
+        '10-20' => '10-20tr',
+        '20-50' => '20-50tr',
+        '50-100' =>'50-100tr',
+        '100-10000' => '> 100tr'        ];
+
+        @endphp
+        @foreach ($khoanggias AS $khoanggia => $kname)
+        <a data-remove="{{$khoanggia}}"
+
+           @if(\Illuminate\Support\Facades\Input::get('gia') == $khoanggia)
+
+        class="active"
+        @endif
+
+
+        href="{{URL::current()."?page=".(\Illuminate\Support\Facades\Input::get('page') ?? 0)."&".http_build_query(['mang' =>  (\Illuminate\Support\Facades\Input::get('mang') ?? '') , 'dauso' =>  (\Illuminate\Support\Facades\Input::get('dauso') ?? ''), 'gia' =>  $khoanggia, 'sapxep' => (\Illuminate\Support\Facades\Input::get('sapxep') ?? '')])}}">{{$kname}}</a>
+
+        @endforeach
+
+
+    </div>
+    <div class="dropdown">
+        <button class="dropbtn">
+            @if(\Illuminate\Support\Facades\Input::get('sapxep') =='ngaunhien')
+            Ngẫu nhiên
+            @elseif(\Illuminate\Support\Facades\Input::get('sapxep') =='giathap')
+            Giá thấp đến cao
+            @elseif(\Illuminate\Support\Facades\Input::get('sapxep') =='giacao')
+            Giá cao đến thấp
+            @else
+            Sắp xếp
+            @endif
+            <span class="caret"></span>
+        </button>
+        <div class="dropdown-content">
+            <a @if(\Illuminate\Support\Facades\Input::get('sapxep') =='ngaunhien')
+
+            class="active"
+            @endif
+
+
+            href="{{URL::current()."?page=".(\Illuminate\Support\Facades\Input::get('page') ?? 0)."&".http_build_query(['mang' =>  (\Illuminate\Support\Facades\Input::get('mang') ?? '') , 'dauso' =>  (\Illuminate\Support\Facades\Input::get('dauso') ?? ''), 'gia' => (\Illuminate\Support\Facades\Input::get('gia') ?? ''), 'sapxep' => 'ngaunhien','sim' => (\Illuminate\Support\Facades\Input::get('sim') ?? ''),  'loai' => (\Illuminate\Support\Facades\Input::get('loai') ?? '')])}}">Ngẫu
+            nhiên</a>
+            <a
+                @if(\Illuminate\Support\Facades\Input::get('sapxep') =='giathap')
+
+            class="active"
+            @endif
+
+
+            href="{{URL::current()."?page=".(\Illuminate\Support\Facades\Input::get('page') ?? 0)."&".http_build_query(['mang' =>  (\Illuminate\Support\Facades\Input::get('mang') ?? '') , 'dauso' =>  (\Illuminate\Support\Facades\Input::get('dauso') ?? ''), 'gia' => (\Illuminate\Support\Facades\Input::get('gia') ?? ''), 'sapxep' => 'giathap','sim' => (\Illuminate\Support\Facades\Input::get('sim') ?? ''),  'loai' => (\Illuminate\Support\Facades\Input::get('loai') ?? '')])}}">Giá
+            thấp đến cao</a>
+            <a
+
+                @if(\Illuminate\Support\Facades\Input::get('sapxep') =='giacao')
+
+            class="active"
+            @endif
+
+
+            href="{{URL::current()."?page=".(\Illuminate\Support\Facades\Input::get('page') ?? 0)."&".http_build_query(['mang' =>  (\Illuminate\Support\Facades\Input::get('mang') ?? '') , 'dauso' =>  (\Illuminate\Support\Facades\Input::get('dauso') ?? ''), 'gia' => (\Illuminate\Support\Facades\Input::get('gia') ?? ''), 'sapxep' => 'giacao','sim' => (\Illuminate\Support\Facades\Input::get('sim') ?? ''),  'loai' => (\Illuminate\Support\Facades\Input::get('loai') ?? '')])}}">Giá
+            cao đến thấp</a>
+        </div>
+    </div>
+
+
+</div>
